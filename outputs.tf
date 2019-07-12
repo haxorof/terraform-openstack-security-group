5bd90e93990a314909ac8a9f03c7ad08115c3d98
Bjorn Oscarsson <haxorof@users.noreply.github.com>
2019-07-12 19:38:37 +0200

    Removed security group description from outputs

--- outputs.tf
+++ outputs.tf
@@ -13,11 +13,3 @@ output "security_group_name" {
     [""],
   )[0]
 }
-
-output "security_group_description" {
-  description = "The description of the security group"
-  value = concat(
-    openstack_networking_secgroup_v2.this.*.description,
-    [""],
-  )[0]
-}
